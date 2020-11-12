9c693a36cd0cc35e01ed8bc51e7b9643e2248c77
clouderow <clouderow@gmail.com>
2020-11-12 15:40:38 +0800

    undo save config

--- disentanglement_lib/methods/unsupervised/train.py
+++ disentanglement_lib/methods/unsupervised/train.py
@@ -35,25 +35,25 @@ def train_with_gin(model_dir,
                    overwrite=False,
                    gin_config_files=None,
                    gin_bindings=None):
-  """Trains a model based on the provided gin configuration.
+    """Trains a model based on the provided gin configuration.
 
-  This function will set the provided gin bindings, call the train() function
-  and clear the gin config. Please see train() for required gin bindings.
+    This function will set the provided gin bindings, call the train() function
+    and clear the gin config. Please see train() for required gin bindings.
 
-  Args:
-    model_dir: String with path to directory where model output should be saved.
-    overwrite: Boolean indicating whether to overwrite output directory.
-    gin_config_files: List of gin config files to load.
-    gin_bindings: List of gin bindings to use.
-  """
-  if gin_config_files is None:
-    gin_config_files = []
-  if gin_bindings is None:
-    gin_bindings = []
-  gin.parse_config_files_and_bindings(gin_config_files, gin_bindings)
+    Args:
+      model_dir: String with path to directory where model output should be saved.
+      overwrite: Boolean indicating whether to overwrite output directory.
+      gin_config_files: List of gin config files to load.
+      gin_bindings: List of gin bindings to use.
+    """
+    if gin_config_files is None:
+        gin_config_files = []
+    if gin_bindings is None:
+        gin_bindings = []
+    gin.parse_config_files_and_bindings(gin_config_files, gin_bindings)
 
-  train(model_dir, overwrite)
-  gin.clear_config()
+    train(model_dir, overwrite)
+    gin.clear_config()
 
 
 @gin.configurable("model", blacklist=["model_dir", "overwrite"])
@@ -66,113 +66,109 @@ def train(model_dir,
           eval_steps=1000,
           name="",
           model_num=None):
-  """Trains the estimator and exports the snapshot and the gin config.
+    """Trains the estimator and exports the snapshot and the gin config.
 
-  The use of this function requires the gin binding 'dataset.name' to be
-  specified as that determines the data set used for training.
+    The use of this function requires the gin binding 'dataset.name' to be
+    specified as that determines the data set used for training.
 
-  Args:
-    model_dir: String with path to directory where model output should be saved.
-    overwrite: Boolean indicating whether to overwrite output directory.
-    model: GaussianEncoderModel that should be trained and exported.
-    training_steps: Integer with number of training steps.
-    random_seed: Integer with random seed used for training.
-    batch_size: Integer with the batch size.
-    eval_steps: Optional integer with number of steps used for evaluation.
-    name: Optional string with name of the model (can be used to name models).
-    model_num: Optional integer with model number (can be used to identify
-      models).
-  """
-  # We do not use the variables 'name' and 'model_num'. Instead, they can be
-  # used to name results as they will be part of the saved gin config.
-  del name, model_num
+    Args:
+      model_dir: String with path to directory where model output should be saved.
+      overwrite: Boolean indicating whether to overwrite output directory.
+      model: GaussianEncoderModel that should be trained and exported.
+      training_steps: Integer with number of training steps.
+      random_seed: Integer with random seed used for training.
+      batch_size: Integer with the batch size.
+      eval_steps: Optional integer with number of steps used for evaluation.
+      name: Optional string with name of the model (can be used to name models).
+      model_num: Optional integer with model number (can be used to identify
+        models).
+    """
+    # We do not use the variables 'name' and 'model_num'. Instead, they can be
+    # used to name results as they will be part of the saved gin config.
+    del name, model_num
 
-  # Delete the output directory if it already exists.
-  if tf.gfile.IsDirectory(model_dir):
-    if overwrite:
-      tf.gfile.DeleteRecursively(model_dir)
-    else:
-      raise ValueError("Directory already exists and overwrite is False.")
-    os.mkdir(model_dir)
+    # Delete the output directory if it already exists.
+    if tf.gfile.IsDirectory(model_dir):
+        if overwrite:
+            tf.gfile.DeleteRecursively(model_dir)
+        else:
+            raise ValueError("Directory already exists and overwrite is False.")
 
-  with open(os.path.join(model_dir, 'config.gin'), 'w+') as file:
-      file.write(gin.operative_config_str())
+    # Create a numpy random state. We will sample the random seeds for training
+    # and evaluation from this.
+    random_state = np.random.RandomState(random_seed)
 
-  # Create a numpy random state. We will sample the random seeds for training
-  # and evaluation from this.
-  random_state = np.random.RandomState(random_seed)
+    # Obtain the dataset.
+    dataset = named_data.get_named_ground_truth_data()
 
-  # Obtain the dataset.
-  dataset = named_data.get_named_ground_truth_data()
+    # We create a TPUEstimator based on the provided model. This is primarily so
+    # that we could switch to TPU training in the future. For now, we train
+    # locally on GPUs.
+    save_checkpoints_steps = training_steps // 10
+    run_config = contrib_tpu.RunConfig(
+        tf_random_seed=random_seed,
+        keep_checkpoint_max=11,
+        save_checkpoints_steps=save_checkpoints_steps,
+        save_summary_steps=save_checkpoints_steps,
+        tpu_config=contrib_tpu.TPUConfig(iterations_per_loop=500))
 
-  # We create a TPUEstimator based on the provided model. This is primarily so
-  # that we could switch to TPU training in the future. For now, we train
-  # locally on GPUs.
-  save_checkpoints_steps = training_steps // 10
-  run_config = contrib_tpu.RunConfig(
-      tf_random_seed=random_seed,
-      keep_checkpoint_max=11,
-      save_checkpoints_steps=save_checkpoints_steps,
-      save_summary_steps=save_checkpoints_steps,
-      tpu_config=contrib_tpu.TPUConfig(iterations_per_loop=500))
+    # check the existence of tpu
+    is_tpu = None
+    try:
+        resolver = tf.distribute.cluster_resolver.TPUClusterResolver()
+        tf.config.experimental_connect_to_cluster(resolver)
+        tf.tpu.experimental.initialize_tpu_system(resolver)
+        print("All devices: ", tf.config.list_logical_devices('TPU'))
+        is_tpu = True
+    except ValueError:
+        is_tpu = False
 
-  # check the existence of tpu
-  is_tpu = None
-  try:
-      resolver = tf.distribute.cluster_resolver.TPUClusterResolver()
-      tf.config.experimental_connect_to_cluster(resolver)
-      tf.tpu.experimental.initialize_tpu_system(resolver)
-      print("All devices: ", tf.config.list_logical_devices('TPU'))
-      is_tpu = True
-  except ValueError:
-      is_tpu = False
+    tpu_estimator = contrib_tpu.TPUEstimator(
+        use_tpu=is_tpu,
+        model_fn=model.model_fn,
+        model_dir=os.path.join(model_dir, "tf_checkpoint"),
+        train_batch_size=batch_size,
+        eval_batch_size=batch_size,
+        config=run_config)
 
-  tpu_estimator = contrib_tpu.TPUEstimator(
-      use_tpu=is_tpu,
-      model_fn=model.model_fn,
-      model_dir=os.path.join(model_dir, "tf_checkpoint"),
-      train_batch_size=batch_size,
-      eval_batch_size=batch_size,
-      config=run_config)
+    # Set up time to keep track of elapsed time in results.
+    experiment_timer = time.time()
 
-  # Set up time to keep track of elapsed time in results.
-  experiment_timer = time.time()
+    # Do the actual training.
+    tpu_estimator.train(
+        input_fn=_make_input_fn(dataset, random_state.randint(2 ** 32)),
+        steps=training_steps)
 
-  # Do the actual training.
-  tpu_estimator.train(
-      input_fn=_make_input_fn(dataset, random_state.randint(2**32)),
-      steps=training_steps)
+    # Save model as a TFHub module.
+    output_shape = named_data.get_named_ground_truth_data().observation_shape
+    module_export_path = os.path.join(model_dir, "tfhub")
+    gaussian_encoder_model.export_as_tf_hub(model, output_shape,
+                                            tpu_estimator.latest_checkpoint(),
+                                            module_export_path)
 
-  # Save model as a TFHub module.
-  output_shape = named_data.get_named_ground_truth_data().observation_shape
-  module_export_path = os.path.join(model_dir, "tfhub")
-  gaussian_encoder_model.export_as_tf_hub(model, output_shape,
-                                          tpu_estimator.latest_checkpoint(),
-                                          module_export_path)
-
-  # Save the results. The result dir will contain all the results and config
-  # files that we copied along, as we progress in the pipeline. The idea is that
-  # these files will be available for analysis at the end.
-  results_dict = tpu_estimator.evaluate(
-      input_fn=_make_input_fn(
-          dataset, random_state.randint(2**32), num_batches=eval_steps))
-  results_dir = os.path.join(model_dir, "results")
-  results_dict["elapsed_time"] = time.time() - experiment_timer
-  results.update_result_directory(results_dir, "train", results_dict)
+    # Save the results. The result dir will contain all the results and config
+    # files that we copied along, as we progress in the pipeline. The idea is that
+    # these files will be available for analysis at the end.
+    results_dict = tpu_estimator.evaluate(
+        input_fn=_make_input_fn(
+            dataset, random_state.randint(2 ** 32), num_batches=eval_steps))
+    results_dir = os.path.join(model_dir, "results")
+    results_dict["elapsed_time"] = time.time() - experiment_timer
+    results.update_result_directory(results_dir, "train", results_dict)
 
 
 def _make_input_fn(ground_truth_data, seed, num_batches=None):
-  """Creates an input function for the experiments."""
+    """Creates an input function for the experiments."""
 
-  def load_dataset(params):
-    """TPUEstimator compatible input fuction."""
-    dataset = util.tf_data_set_from_ground_truth_data(ground_truth_data, seed)
-    batch_size = params["batch_size"]
-    # We need to drop the remainder as otherwise we lose the batch size in the
-    # tensor shape. This has no effect as our data set is infinite.
-    dataset = dataset.batch(batch_size, drop_remainder=True)
-    if num_batches is not None:
-      dataset = dataset.take(num_batches)
-    return dataset.make_one_shot_iterator().get_next()
+    def load_dataset(params):
+        """TPUEstimator compatible input fuction."""
+        dataset = util.tf_data_set_from_ground_truth_data(ground_truth_data, seed)
+        batch_size = params["batch_size"]
+        # We need to drop the remainder as otherwise we lose the batch size in the
+        # tensor shape. This has no effect as our data set is infinite.
+        dataset = dataset.batch(batch_size, drop_remainder=True)
+        if num_batches is not None:
+            dataset = dataset.take(num_batches)
+        return dataset.make_one_shot_iterator().get_next()
 
-  return load_dataset
+    return load_dataset
